dced802095a5225375e32ab68f7688eae55ad7b4
lundaljung <43650954+w-ryan-jung@users.noreply.github.com>
2024-10-04 22:55:29 +0900

    update

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,29 @@
-import express from 'express';
-import sessionRouter from './router/sessions.js';
-import attendanceRouter from './router/attendance.js';
-import authRouter from './router/auth.js';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import sessionRouter from "./router/sessions.js";
+import attendanceRouter from "./router/attendance.js";
+import authRouter from "./router/auth.js";
+import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
-const MONGO_URI = 'mongodb+srv://lundaljung:dOSzMl88gH7e0ep5@cluster0.ooy7u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/UoPS';
+const MONGO_URI =
+  "mongodb+srv://lundaljung:dOSzMl88gH7e0ep5@cluster0.ooy7u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/UoPS";
 
 const server = async () => {
   try {
     await mongoose.connect(MONGO_URI);
-    console.log('DB Connected');
+    console.log("DB Connected");
+
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+    });
 
     app.use(express.json());
     app.use(cors());
-    app.use('/attendance', attendanceRouter);
-    app.use('/sessions', sessionRouter);
-    app.use('/auth', authRouter);
+    app.use("/attendance", attendanceRouter);
+    app.use("/sessions", sessionRouter);
+    app.use("/auth", authRouter);
 
     app.use((req, res, next) => {
       res.sendStatus(404);
@@ -27,8 +33,8 @@ const server = async () => {
       res.sendStatus(500);
     });
 
-    app.listen(8000, () => {
-      console.log('Server listning on port 8080');
+    app.listen(8080, () => {
+      console.log("Server listning on port 8080");
     });
   } catch (error) {
     console.error(error);
